ae7c95f2c6e382f63bb53c831352c1744b74cb23
Abby Schneider <abbyschneider612@gmail.com>
2024-06-12 21:22:05 -0500

    drawing some embers or something

--- sketch.js
+++ sketch.js
@@ -1,22 +1,80 @@
+let record = false;
+
+let startingFrame   = 60;
+let recordingLength = 5; // in seconds
+
+let ballCount = 200;
+
+let x = [];
+let y = [];
+let size = [];
+let xSpeed = [];
+let ySpeed = [];
+let r = [];
+let g = [];
+let b = [];
+
+let resolutionX = 360;
+let resolutionY = 800;
+
 function setup(){
     // put setup code here
+    createCanvas(windowWidth, windowHeight);
 
-    createCanvas(500, 400);
+    for (let i =0; i < ballCount; i++){
+        x[i] = randomGaussian(0.5*width, 50);
+        y[i] = randomGaussian(0.75*height, 50);
+
+        xSpeed[i] = random(-1, 1);
+        ySpeed[i] = random(-1, 1);
+
+        size[i] = random(20,40);
+        r[i] = random(220,255);
+        g[i] = random(80,140);
+        b[i] = random(0,40);
+    }
 }
 
 function draw(){
     // put drawing code here
-    background(220);
-    stroke(200);
-    line(0, height/2, width, height/2);
-    line(width/2, 0, width/2, height);
+    if (record && frameCount === startingFrame){
+        capturer.start()
+    }
 
-    fill('#235456');
-    circle(width/4, 3*height/4, 125);
+    background(35, 60);
 
-    fill('lavender');
-    circle(3.2*width/4, 1.2*height/4, 125);
+    blendMode(BLEND);
+    for(let i=0; i < ballCount; i++){
+        
+        x[i] = x[i] + xSpeed[i]
+        y[i] = y[i] + -randomGaussian(0.6, 1)
 
-    fill('turquoise')
-    square(3*width/4, 3*height/4, 65, 15)
+        // make the balls "bounce" off the edge
+        if(x[i] < 0 || x[i] > width){
+            xSpeed[i] = -xSpeed[i]
+        }
+        // 
+        if(y[i] < 0 || y[i] > height){
+            ySpeed[i] = -ySpeed[i]
+        }
+
+        fill(r[i], g[i], b[i], 60);
+        ellipse(x[i], y[i], random(0.4,0.45)*size[i], random(0.4,0.5)*size[i]);
+        
+        fill(r[i], g[i], b[i], random(4,20));
+        noStroke();
+        ellipse(x[i], y[i], size[i], size[i]);
+     
+    }
+
+    if (record && frameCount < startingFrame + 60*recordingLength) {
+        capturer.capture(canvas)
+    } else if (record && frameCount === startingFrame + 60*recordingLength) {
+        capturer.save()
+        capturer.stop()
+    }
+}
+
+function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
 }
